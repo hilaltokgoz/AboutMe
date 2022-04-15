e88b3aace4f0501dd62925b0b4fda8fe10d1eb79
Hilal <hilaltokgoz98@gmail.com>
2022-04-15 13:29:54 +0300

    End Commit

--- app/src/main/java/com/aboutme/MainActivity.kt
+++ app/src/main/java/com/aboutme/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.aboutme
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -15,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.doneButton).setOnClickListener {
             addNickname(it) //Butonu verir
         }
+        findViewById<TextView>(R.id.nicknameText).setOnClickListener {
+            updateNickname(it)
+        }
     }
-
 
     //Butona basıldığında çalışacak fonksiyon
     private fun addNickname(view: View) {
@@ -27,5 +31,28 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nicknameText.visibility = View.VISIBLE
+
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(
+            view.windowToken,
+            0
+        ) //butondan sonra klavye gizlenir.
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickNameEdit)
+        val doneButton = findViewById<Button>(R.id.doneButton)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()   //textView e basılınca editText görünür olsun ve klavye açılsın.
+
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText, 0)
+
     }
 }
