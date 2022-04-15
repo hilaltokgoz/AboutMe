1fb89e574fd81bc1248f091f5866f74c531ed102
Hilal <hilaltokgoz98@gmail.com>
2022-04-15 13:09:36 +0300

    UI Update Commit

--- app/src/main/java/com/aboutme/MainActivity.kt
+++ app/src/main/java/com/aboutme/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //xml ile kt yi bağla. R,kaynağa referanstır.
 
+        findViewById<Button>(R.id.doneButton).setOnClickListener {
+            addNickname(it) //Butonu verir
+        }
+    }
+
+
+    //Butona basıldığında çalışacak fonksiyon
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickNameEdit)
+        val nicknameText = findViewById<TextView>(R.id.nicknameText)
+
+        nicknameText.text = editText.text //editText'e girilen text'i yerleştir.
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
     }
 }
