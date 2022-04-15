f9327910a1b16e471ab0cb87264a6ec3b69ada0d
Hilal <hilaltokgoz98@gmail.com>
2022-04-15 11:51:20 +0300

    Initial commit

---
+++ app/src/main/java/com/aboutme/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //xml ile kt yi bağla. R,kaynağa referanstır.
+
+    }
+}
