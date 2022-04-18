e2432adee4f7a56792f0633bd51e40828b687267
Hilal <hilaltokgoz98@gmail.com>
2022-04-18 13:42:51 +0300

    DataBinding with Update Commit

---
+++ app/src/main/java/com/MyName.kt
@@ -0,0 +1,13 @@
+package com
+
+
+// Code with ❤️
+//┌──────────────────────────┐
+//│ Created by Hilal TOKGOZ  │
+//│ ──────────────────────── │
+//│ hilaltokgoz98@gmail.com  │
+//│ ──────────────────────── │
+//│ 18.04.2022               │
+//└──────────────────────────┘
+
+data class MyName(var name: String = "", var nickname: String = "")
--- app/src/main/java/com/aboutme/MainActivity.kt
+++ app/src/main/java/com/aboutme/MainActivity.kt
@@ -4,34 +4,50 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.MyName
+import com.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private val myName:MyName= MyName("Aleks Haecky")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //xml ile kt yi bağla. R,kaynağa referanstır.
-
-        findViewById<Button>(R.id.doneButton).setOnClickListener {
+        //dataBinding
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setContentView(binding.root) //xml ile kt yi bağla. R,kaynağa referanstır.
+        binding.apply {
+            doneButton.setOnClickListener { addNickname(it) }
+            nicknameText.setOnClickListener { updateNickname(it) }
+        }
+        binding.myNameObject=myName //oluşturulan nesne objeye verildi
+        //findViewById ile yapılışı
+      /*  findViewById<Button>(R.id.doneButton).setOnClickListener {
             addNickname(it) //Butonu verir
         }
         findViewById<TextView>(R.id.nicknameText).setOnClickListener {
             updateNickname(it)
-        }
+        }*/
     }
-
     //Butona basıldığında çalışacak fonksiyon
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickNameEdit)
-        val nicknameText = findViewById<TextView>(R.id.nicknameText)
-
-        nicknameText.text = editText.text //editText'e girilen text'i yerleştir.
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
-
+        //findViewById
+        /*   val editText = findViewById<EditText>(R.id.nickNameEdit)
+           val nicknameText = findViewById<TextView>(R.id.nicknameText)
+           nicknameText.text = editText.text //editText'e girilen text'i yerleştir.
+           editText.visibility = View.GONE
+           view.visibility = View.GONE
+           nicknameText.visibility = View.VISIBLE*/
+        //Binding
+        binding.apply {
+            myName.nickname=binding.nickNameEdit.text.toString()
+            invalidateAll() //Nesneyi değiştirince UI yı update et
+            doneButton.visibility=View.GONE
+            nickNameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(
@@ -41,18 +57,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickNameEdit)
-        val doneButton = findViewById<Button>(R.id.doneButton)
+        //findViewById
+        /* val editText = findViewById<EditText>(R.id.nickNameEdit)
+         val doneButton = findViewById<Button>(R.id.doneButton)
+         editText.visibility = View.VISIBLE
+         doneButton.visibility = View.VISIBLE
+         view.visibility = View.GONE
 
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
+         editText.requestFocus() */  //textView e basılınca editText görünür olsun ve klavye açılsın.
+        binding.apply {
+            nickNameEdit.visibility = View.VISIBLE
+            doneButton.visibility = View.VISIBLE
+
+            nickNameEdit.requestFocus()
+        }
         view.visibility = View.GONE
 
-        editText.requestFocus()   //textView e basılınca editText görünür olsun ve klavye açılsın.
 
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.showSoftInput(editText, 0)
+        inputMethodManager.showSoftInput(binding.nickNameEdit, 0)
 
     }
 }
